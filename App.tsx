b348f34a7b47bf3a30a16cd65e3466ae68c7c1c5
Magnús Ólafsson <magnusol93@gmail.com>
2024-05-14 18:39:47 +0000

    Add Biome

--- App.tsx
+++ App.tsx
@@ -1,47 +1,54 @@
-import { useState } from 'react';
-import { StatusBar } from 'expo-status-bar';
-import { KeyboardAvoidingView, StyleSheet, Text, TextInput, View } from 'react-native';
+import { useState } from "react";
+import { StatusBar } from "expo-status-bar";
+import {
+	KeyboardAvoidingView,
+	StyleSheet,
+	Text,
+	TextInput,
+	View,
+} from "react-native";
 
 export default function App() {
-  const [count, setCount] = useState(0)
+	const [count, setCount] = useState(0);
 
-  return (
-    <View style={styles.container}>
-      <Text style={styles.title}>Hallo!</Text>
-      <Text style={styles.title} onPress={() => {
-        setCount(count + 1)
-      }}>{count}</Text>
-      <KeyboardAvoidingView behavior="padding">
-      <View style={styles.inputContainer}>
-        <TextInput 
-          style={styles.input}
-          placeholder="Fara út með ruslið..."
-        />
-      </View>
-      </KeyboardAvoidingView>
-      <StatusBar style="auto" />
-    </View>
-  );
+	return (
+		<View style={styles.container}>
+			<Text style={styles.title}>Hallo!</Text>
+			<Text
+				style={styles.title}
+				onPress={() => {
+					setCount(count + 1);
+				}}
+			>
+				{count}
+			</Text>
+			<KeyboardAvoidingView behavior="padding">
+				<View style={styles.inputContainer}>
+					<TextInput style={styles.input} placeholder="Fara út með ruslið..." />
+				</View>
+			</KeyboardAvoidingView>
+			<StatusBar style="auto" />
+		</View>
+	);
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: 'orange',
-    justifyContent: 'flex-end',
-  },
-  title: {
-    fontSize: 80,
-    fontWeight: 'bold',
-  },
-  inputContainer: {
-    padding: 20,
-    backgroundColor: 'white',
-    borderTopLeftRadius: 20,
-    borderTopRightRadius: 20,
-  },
-    input: {
-    borderWidth: 1,
-
-}
+	container: {
+		flex: 1,
+		backgroundColor: "orange",
+		justifyContent: "flex-end",
+	},
+	title: {
+		fontSize: 80,
+		fontWeight: "bold",
+	},
+	inputContainer: {
+		padding: 20,
+		backgroundColor: "white",
+		borderTopLeftRadius: 20,
+		borderTopRightRadius: 20,
+	},
+	input: {
+		borderWidth: 1,
+	},
 });
